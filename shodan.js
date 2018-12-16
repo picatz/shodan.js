09e723a0ea273b8590eae6ac70b0c52b79103369
Kent 'picat' Gruber <kgruber1@emich.edu>
2018-12-16 12:31:51 -0500

    Update shodan.js

--- shodan.js
+++ shodan.js
@@ -1,4 +1,3 @@
-
 class ShodanClient {
     constructor(api_key) {
         this.api_key = api_key;
@@ -9,6 +8,8 @@ class ShodanClient {
             defaultAPI: "https://api.shodan.io"
         }
     }
+
+    // REST API
 
     _turn_into_facets(params) {
         if (params) {
@@ -114,11 +115,359 @@ class ShodanClient {
         let response = await fetch(url)
 
         if (response.ok) return await response.json()
-        
+
         throw new Error(response.status)
     }
+
+    async scan(ips) {
+        let url = new URL(this.endpoints.defaultAPI + "/shodan/scan")
+
+        url.searchParams.append("ips", ips)
+
+        url.searchParams.append("key", this.api_key)
+
+        let response = await fetch(url, { method: "POST" })
+
+        if (response.ok) return await response.json()
+
+        throw new Error(response.status)
+    }
+
+    async scanInternet(params) {
+        let url = new URL(this.endpoints.defaultAPI + "/shodan/scan/internet")
+
+        if (params) { Object.keys(params).forEach(key => url.searchParams.append(key, params[key])) }
+
+        url.searchParams.append("key", this.api_key)
+
+        let response = await fetch(url, { method: "POST" })
+
+        if (response.ok) return await response.json()
+
+        throw new Error(response.status)
+    }
+
+    async scanStatus(id) {
+        let url = new URL(this.endpoints.defaultAPI + "/shodan/scan/" + id)
+
+        url.searchParams.append("id", id)
+
+        url.searchParams.append("key", this.api_key)
+
+        let response = await fetch(url)
+
+        if (response.ok) return await response.json()
+
+        throw new Error(response.status)
+    }
+
+    async alert(params) {
+        let url = new URL(this.endpoints.defaultAPI + "/shodan/alert")
+
+        if (params) { Object.keys(params).forEach(key => url.searchParams.append(key, params[key])) }
+
+        url.searchParams.append("key", this.api_key)
+
+        let response = await fetch(url, { method: "POST" })
+
+        if (response.ok) return await response.json()
+
+        throw new Error(response.status)
+    }
+
+    async alertInfo(id) {
+        let url = new URL(this.endpoints.defaultAPI + "/shodan/alert/" + id + "/info")
+
+        url.searchParams.append("key", this.api_key)
+
+        let response = await fetch(url)
+
+        if (response.ok) return await response.json()
+
+        throw new Error(response.status)
+    }
+
+    async deleteAlert(id) {
+        let url = new URL(this.endpoints.defaultAPI + "/shodan/alert/" + id + "/info")
+
+        url.searchParams.append("key", this.api_key)
+
+        let response = await fetch(url, { method: "DELETE" })
+
+        if (response.ok) return await response.json()
+
+        throw new Error(response.status)
+    }
+
+    async alerts() {
+        let url = new URL(this.endpoints.defaultAPI + "/shodan/alert/info")
+
+        url.searchParams.append("key", this.api_key)
+
+        let response = await fetch(url)
+
+        if (response.ok) return await response.json()
+
+        throw new Error(response.status)
+    }
+
+    async communityQueryList(params) {
+        let url = new URL(this.endpoints.defaultAPI + "/shodan/query")
+
+        if (params) { Object.keys(params).forEach(key => url.searchParams.append(key, params[key])) }
+
+        url.searchParams.append("key", this.api_key)
+
+        let response = await fetch(url)
+
+        if (response.ok) return await response.json()
+
+        throw new Error(response.status)
+    }
+
+    async communityQuerySearch(query, page) {
+        let url = new URL(this.endpoints.defaultAPI + "/shodan/query/search")
+
+        if (page) {
+            url.searchParams.append("page", page)
+        }
+
+        url.searchParams.append("key", this.api_key)
+
+        let response = await fetch(url)
+
+        if (response.ok) return await response.json()
+
+        throw new Error(response.status)
+    }
+
+    async communityPopularQueryTags(size) {
+        let url = new URL(this.endpoints.defaultAPI + "/shodan/query/tags")
+
+        if (size) {
+            url.searchParams.append("size", size)
+        }
+
+        url.searchParams.append("key", this.api_key)
+
+        let response = await fetch(url)
+
+        if (response.ok) return await response.json()
+
+        throw new Error(response.status)
+    }
+
+    async accountInfo() {
+        let url = new URL(this.endpoints.defaultAPI + "/account/profile")
+
+        url.searchParams.append("key", this.api_key)
+
+        let response = await fetch(url)
+
+        if (response.ok) return await response.json()
+
+        throw new Error(response.status)
+    }
+
+    async resolve(hosts) {
+        let url = new URL(this.endpoints.defaultAPI + "/dns/resolve")
+
+        url.searchParams.append("hostnames", hosts)
+
+        url.searchParams.append("key", this.api_key)
+
+        let response = await fetch(url)
+
+        if (response.ok) return await response.json()
+
+        throw new Error(response.status)
+    }
+
+    async reverseLookup(ips) {
+        let url = new URL(this.endpoints.defaultAPI + "/dns/reverse")
+
+        url.searchParams.append("ips", ips)
+
+        url.searchParams.append("key", this.api_key)
+
+        let response = await fetch(url)
+
+        if (response.ok) return await response.json()
+
+        throw new Error(response.status)
+    }
+
+    async httpHeaders() {
+        let url = new URL(this.endpoints.defaultAPI + "/tools/httpheaders")
+
+        url.searchParams.append("key", this.api_key)
+
+        let response = await fetch(url)
+
+        if (response.ok) return await response.json()
+
+        throw new Error(response.status)
+    }
+
+    async myIP() {
+        let url = new URL(this.endpoints.defaultAPI + "/tools/myip")
+
+        url.searchParams.append("key", this.api_key)
+
+        let response = await fetch(url)
+
+        if (response.ok) return await response.json()
+
+        throw new Error(response.status)
+    }
+
+    async honeyScore(ip) {
+        let url = new URL(this.endpoints.defaultAPI + "/labs/honeyscore/" + ip)
+
+        url.searchParams.append("key", this.api_key)
+
+        let response = await fetch(url)
+
+        if (response.ok) return await response.json()
+
+        throw new Error(response.status)
+    }
+
+    async planInfo() {
+        let url = new URL(this.endpoints.defaultAPI + "/api-info")
+
+        url.searchParams.append("key", this.api_key)
+
+        let response = await fetch(url)
+
+        if (response.ok) return await response.json()
+
+        throw new Error(response.status)
+    }
+
+    // Exploits API
+
+    async searchExploits(query, facets) {
+        let url = new URL(this.endpoints.exploitAPI + "/search")
+
+        url.searchParams.append("query", query)
+
+        facets = this._turn_into_facets(facets)
+
+        if (facets) {
+            url.searchParams.append("facets", facets)
+        }
+
+        url.searchParams.append("key", this.api_key)
+
+        let response = await fetch(url)
+
+        let json = await response.json()
+
+        if (response.ok) return json
+
+        throw new Error(json.error)
+    }
+
+    async countExploits(query, facets) {
+        let url = new URL(this.endpoints.exploitAPI + "/count")
+
+        url.searchParams.append("query", query)
+
+        facets = this._turn_into_facets(facets)
+
+        if (facets) {
+            url.searchParams.append("facets", facets)
+        }
+
+        url.searchParams.append("key", this.api_key)
+
+        let response = await fetch(url)
+
+        let json = await response.json()
+
+        if (response.ok) return json
+
+        throw new Error(json.error)
+    }
+
+    // Streaming API
+
+    _slurp_stream(url, callback, interval) {
+        url.searchParams.append("t", "json");
+
+        url.searchParams.append("key", this.api_key);
+
+        if (!interval) {
+            interval = 1000
+        }
+
+        var reader = null
+
+        fetch(url).then(function (response) {
+            reader = response.body.getReader();
+        });
+
+        let buffer = [];
+
+        let readerInterval = setInterval(function () {
+            if (reader) {
+                reader.read().then(function (data) {
+                    if (data.done) {
+                        clearInterval(readerInterval);
+                        return
+                    }
+                    data.value.forEach(function (e) {
+                        buffer.push(e);
+                        if (e == 10) {
+                            var json = JSON.parse(new TextDecoder("utf-8").decode(Uint8Array.from(buffer)));
+                            if (callback) {
+                                callback(json)
+                            }
+                            buffer = [];
+                        }
+                    })
+                })
+            } else {
+                clearInterval(readerInterval);
+                return
+            }
+        }, interval)
+    }
+
+    bannerStream(callback, interval) {
+        let url = new URL(this.endpoints.streamingAPI + "/shodan/banners")
+
+        _slurp_stream(url, callback, interval)
+    }
+
+    bannerStreamByASNs(asns, callback, interval) {
+        let url = new URL(this.endpoints.streamingAPI + "/shodan/asn/" + asns)
+
+        _slurp_stream(url, callback, interval)
+    }
+
+    bannerStreamByCountries(countries, callback, interval) {
+        let url = new URL(this.endpoints.streamingAPI + "/shodan/countries/" + countries)
+
+        _slurp_stream(url, callback, interval)
+    }
+
+    bannerStreamByPorts(ports, callback, interval) {
+        let url = new URL(this.endpoints.streamingAPI + "/shodan/ports/" + ports)
+
+        _slurp_stream(url, callback, interval)
+    }
+
+    alertStream(callback, interval) {
+        let url = new URL(this.endpoints.streamingAPI + "/shodan/alert")
+
+        _slurp_stream(url, callback, interval)
+    }
+
+    alertStreamByID(id, callback, interval) {
+        let url = new URL(this.endpoints.streamingAPI + "/shodan/alert/" + id)
+
+        _slurp_stream(url, callback, interval)
+    }
 }
-
-var client = new ShodanClient("YOUR_API_KEY")
-
-var resp = await client.hostCount("apache")
